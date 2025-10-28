ecc345988b21cf11161416b0cc9ea22b540cbbf7
keisha De La Rosa <keishdlr@gmail.com>
2025-10-28 15:25:35 -0400

    final commit

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -2,7 +2,6 @@ package com.pluralsight;
 
 public class SemiTruck extends Vehicle{
 
-
     public SemiTruck(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity) {
         super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
     }
